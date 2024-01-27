639cd4c46e23617b9e1ab5f7648037a55af33cd0
Maaz Khan <maazkk514@gmail.com>
2024-01-27 14:03:22 +0500

    first commit

---
+++ lib/constants/textstyles.dart
@@ -0,0 +1,38 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+TextStyle kHead1White = GoogleFonts.poppins(
+    fontSize: 24.sp, fontWeight: FontWeight.w500, color: Colors.white);
+TextStyle kHead1Black = GoogleFonts.poppins(
+    fontSize: 24.sp, fontWeight: FontWeight.w500, color: Colors.black);
+TextStyle kHead1Blue = GoogleFonts.poppins(
+    fontSize: 24.sp,
+    fontWeight: FontWeight.w500,
+    color: const Color(0xFF518393));
+TextStyle kHead2White = GoogleFonts.poppins(
+    fontSize: 20.sp, fontWeight: FontWeight.w700, color: Colors.white);
+TextStyle kHead2Black = GoogleFonts.poppins(
+    fontSize: 20.sp, fontWeight: FontWeight.w700, color: Colors.black);
+TextStyle kHead2Blue = GoogleFonts.poppins(
+    fontSize: 20.sp,
+    fontWeight: FontWeight.w700,
+    color: const Color(0xFF518393));
+TextStyle kHeadBlack = GoogleFonts.poppins(
+    fontSize: 16.sp, fontWeight: FontWeight.w700, color: Colors.black);
+TextStyle kHead3Blue = GoogleFonts.poppins(
+    fontSize: 16.sp,
+    fontWeight: FontWeight.w700,
+    color: const Color(0xFF518393));
+TextStyle kBody1White = GoogleFonts.poppins(
+    fontSize: 16.sp, fontWeight: FontWeight.w400, color: Colors.white);
+TextStyle kBody1Black = GoogleFonts.poppins(
+    fontSize: 16.sp, fontWeight: FontWeight.w400, color: Colors.black);
+TextStyle kBody1Transparent = GoogleFonts.poppins(
+    fontSize: 16.sp,
+    fontWeight: FontWeight.w400,
+    color: const Color(0xFFA7A7A7));
+TextStyle kBody2White = GoogleFonts.poppins(
+    fontSize: 12.sp, fontWeight: FontWeight.w400, color: Colors.white);
+TextStyle kBody2Black = GoogleFonts.poppins(
+    fontSize: 12.sp, fontWeight: FontWeight.w400, color: Colors.black);
